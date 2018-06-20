f94129feb7e0b5fef2eb41b40efd73810071226f
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-06-20 20:16:50 -0300

    Piwik adicionado

--- app/layout.php
+++ app/layout.php
@@ -31,5 +31,22 @@
             <div class="g3"></div>
         </div>
     </div>
+    <!-- Matomo -->
+    <script type="text/javascript">
+    var _paq = _paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["setCookieDomain", "*.hackergaucho.com.br"]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//piwik.mushape.com/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+</script>
+<noscript><p><img src="//piwik.mushape.com/piwik.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Matomo Code -->
 </body>
 </html>
